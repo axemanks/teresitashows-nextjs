b5459b1f416e9c9ca27e36be53976bacff4ff5e2
KeithScheldt <keithscheldt@gmail.com>
2023-11-05 18:59:47 -0500

    moved video to flex div

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,72 +13,52 @@ const Hero = (props: Props) => {
   return (
     <section
       id='hero'
-      className='container w-full h-screen sm:nav-padding flex flex-wrap '
+      className='container w-full h-screen flex flex-wrap '
     >
-
-       {/* Hover Contact */}
-     <div className='mx-auto flex w-full items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-5  shadow-md backdrop-blur transition-all hover:border-primary hover:bg-white/50 text-primary'>
+      {/* Hover Contact */}
+      <div className='mx-auto flex w-full items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-5  shadow-md backdrop-blur transition-all hover:border-primary hover:bg-white/50 text-primary'>
         <div className='mr-2'>
-        <PhoneForwardedIcon size={24} />
+          <PhoneForwardedIcon size={24} />
         </div>
-        Contáctanos al 
+        Contáctanos al
         <span>469-328-2513</span>
-     </div>
-
-      {/* bg-video */}
-      <div className=''>
-      <video 
-      src="dancing.mp4" 
-      typeof='video/mp4'
-      playsInline
-      autoPlay 
-      loop
-      muted
-      style={{
-        position: "absolute",
-        width: "100%",
-        height: "100%",
-        zIndex: -10,
-        top: 100,
-        left: 0,        
-      }}
-      />
       </div>
+
       <div className='flex flex-row items-center justify-center space-x-10 flex-wrap '>
-        {/* Image */}       
-        {/* <div className='hidden'>
-        <Image
-          src={teresita}
-          alt='Teresita'
-          width={300}
-          height={300}
-          quality={100}
-          className='rounded-xl'
-          placeholder='blur'
-          aria-label='Teresita'
-        /></div> */}
-       
-        
-        <div className='flex flex-col justify-between flex-grow gap-10'>
-          <h1 className='heading1 text-gradient_blue-orange pt-0 mx-auto text-center'>Estamos De Fiesta!</h1>
-          <p className='mt-60 heading3 text-center text-primary italic mx-auto flex-wrap'>
-          Sabemos que hoy es un día muy importante para ti,  es por eso que siempre ofrecemos  lo mejor para que tu fiesta sea un éxito!
-          </p>
+        <div className='flex flex-col justify-evenly flex-grow gap-10'>
+          <div>
+            <h1 className='heading1 text-gradient_blue-orange pt-0 mx-auto text-center'>
+              Estamos De Fiesta!
+            </h1>
+          </div>
+
+          <div>
+            <video
+              src='dancing.mp4'
+              typeof='video/mp4'
+              playsInline
+              autoPlay
+              loop
+              muted
+              style={{
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+                zIndex: -10,
+                top: 100,
+                left: 0,
+              }}
+            />
+          </div>
+
+          <div>
+            <p className='mt-60 heading3 text-center text-primary italic mx-auto flex-wrap'>
+              Sabemos que hoy es un día muy importante para ti, es por eso que
+              siempre ofrecemos lo mejor para que tu fiesta sea un éxito!
+            </p>
+          </div>
         </div>
-         {/* Image */}       
-        <div className='hidden'>
-        <Image
-          src={teresita}
-          alt='Teresita'
-          width={300}
-          height={300}
-          quality={100}
-          className='rounded-xl'
-          placeholder='blur'
-          aria-label='Teresita'
-        /></div>
-      
-        </div>
+      </div>
     </section>
   );
 };
